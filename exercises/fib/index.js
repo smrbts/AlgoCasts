2462312cb55acb64fefbdf6283b694ccf1e14e7f
smrbts <samueljustinroberts@gmail.com>
2020-05-26 16:25:22 -0500

    about to start linked lists

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,63 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+//Using bottom-up
+function fib(n) {
+    //Base cases
+    if (n <= 2) {
+        return 1;
+    }
+
+    const fibNums = [0, 1, 1];
+
+    for (let i = 3; i <= n; i++) {
+        fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+    }
+
+    return fibNums[n];
+}
 
 module.exports = fib;
+
+
+// NOTE Using memoization
+// function fib(n, memo = []) {
+//     //Base cases
+//     if (memo[n] !== undefined) {
+//         return memo[n];
+//     }
+//     if (n <= 2) {
+//         return 1;
+//     }
+//
+//     const result = fib(n-1, memo) + fib(n-2, memo);
+//
+//     memo[n] = result;
+//
+//     return result;
+// }
+
+// NOTE Recursive version, horrible runtime, O(2^n), without bottom up or memoization
+// function fib(n) {
+        //Base case
+//     if (n < 2) {
+//         return n;
+//     }
+//
+//     return fib(n-1) + fib(n-2);
+// }
+
+// NOTE Iterative version, O(n) linear time
+// function fib(n) {
+//     const result = [0,1];
+//
+//     for (let i = 2; i <= n; i++){
+//         const a = result[i - 1];
+//         const b = result[i - 2];
+//
+//         result.push(a + b);
+//     }
+//
+//     return result[n];
+// }
+
--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,51 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    const results = [];
+    let counter = 1;
+    let startCol = 0;
+    let endCol = n - 1;
+    let startRow = 0;
+    let endRow = n - 1;
+
+    //Tells us how many arrays we will have based on n
+    for (let i = 0; i < n; i++) {
+        results.push([]);
+    }
+
+    while (startCol <= endCol && startRow <= endRow) {
+
+        // Build our top row with this loop
+        for (let i = startCol; i <= endCol; i++) {
+            results[startRow][i] = counter;
+            counter++;
+        }
+        startRow++;
+
+        //This loop builds our right column
+        for (let i = startRow; i <= endRow; i++){
+            results[i][endCol] = counter;
+            counter++;
+        }
+        endCol--;
+
+        //This loop builds the bottom row
+        for (let i = endCol; i >= startCol; i--) {
+            results[endRow][i] = counter;
+            counter++;
+        }
+        endRow--;
+
+        //Left side column
+        for (let i = endRow; i >= startRow; i--) {
+            results[i][startCol] = counter;
+            counter++;
+        }
+        startCol++;
+    }
+
+    return results;
+}
 
 module.exports = matrix;
